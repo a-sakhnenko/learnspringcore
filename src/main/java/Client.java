de9548d6c14de1d5a60b3b9e676b0994dde14ce6
Aleksandr Sakhnenko <Aleksandr_Sakhnenko@epam.com>
2018-04-10 15:43:52 +0300

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,11 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Client {
+    private String id;
+    private String name;
+}
